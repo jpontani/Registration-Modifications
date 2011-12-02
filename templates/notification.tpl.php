30714956c5f3b0fada97fa8d824a2bb10c54ed09
Joe Pontani <jpontani@vt.edu>
2011-12-02 15:00:07 -0500

    by jpontani: scan for template files for notify/reminder email; base notify/reminder template files

---
+++ templates/notification.tpl.php
@@ -0,0 +1,4 @@
+This a notification that someone registered for %event_title on %registration_date.
+
+The following information was submitted:
+%registration
---
+++ templates/reminder.tpl.php
@@ -0,0 +1,6 @@
+Hello %user_name, this is a reminder that you registered for %event_title. This event will take place on %event_date.
+
+If you would like to cancel your registration, just click this link: %registration_cancel_link.
+
+Thank you,
+%site_name Administrator
